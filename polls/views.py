a02a818ef00e41c272750eef895ea9e4845ab17c
xx254 <xx254@cornell.edu>
2021-06-20 17:05:34 -0400

    Added error handling

--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-# import ..
 from youtube.youtube_transcript_api import YouTubeTranscriptApi
 from rest_framework.response import Response
 
@@ -13,10 +12,6 @@ from .serializers import SubtitleSerializer
 from .models import Subtitle
 
 import requests
-
-# file downloading
-
-
 
 class SubtitleViewSet(viewsets.ModelViewSet):
     queryset = Subtitle.objects.all() #.order_by('name') # should be ordered by date
@@ -33,30 +28,41 @@ class SubtitleViewSet(viewsets.ModelViewSet):
          # youtube_url = 'https://www.youtube.com/watch?v=ad9St_ryyBo'
         youtube_url = request.POST.get('youtube_url')
 
-        video_id, subtitles = self.get_subtitles(youtube_url)
+        try:
+            video_id = self.get_video_id(youtube_url)
+            if video_id:
+                subtitles = self.get_subtitles(video_id)
+            else:
+                # if we want to return a string ---
+                # return Response('video id not found')
+                response = HttpResponse('video id not found', content_type='application/vnd.ms-excel')
+                response['Content-Disposition'] = 'attachment; filename="video_id_not_found.txt"'
+                return response # return a file
+        except Exception:
+            return Response('Could not retrieve a transcript for the video. This is most likely caused by - Subtitles are disabled for this video')
 
         Subtitle.objects.create(
             video_id=video_id,
             youtube_url=youtube_url,
             subtitles=subtitles
         )
-        
+
         # generate the file
         response = HttpResponse(subtitles, content_type='application/vnd.ms-excel')
         response['Content-Disposition'] = 'attachment; filename="subtitles.txt"'
 
         return response
-    
         # return Response(subtitles)
 
 
     def get_video_id(self, youtube_url):
+        if not youtube_url:
+            return None
         return youtube_url.split("v=")[1]
 
-    def get_subtitles(self, youtube_url):
-        video_id = self.get_video_id(youtube_url)
+    def get_subtitles(self, video_id):
         subtitle_list = YouTubeTranscriptApi.get_transcript(video_id)
         res = ''
         for s in subtitle_list:
             res += s['text'] + ' '
-        return video_id, res
+        return res
